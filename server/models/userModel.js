31c3bb5aee741fbee90ef5d20a0b269ad2f10582
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2023-04-05 12:17:07 +0200

    add user model

---
+++ server/models/userModel.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose"); // Erase if already required
+const bcrypt = require("bcrypt");
+
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: { type: mongoose.Schema.Types.ObjectId, ref: "Address" },
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    refreshtoken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hashSync(this.password, salt);
+});
+
+userSchema.methods.isPasswordMatch = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//Export the model
+module.exports = mongoose.model("User", userSchema);
