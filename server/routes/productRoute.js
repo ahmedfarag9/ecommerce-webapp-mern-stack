dc6dc3b59e38718c9a40a32e57596efa9e7c2e5c
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2023-04-08 15:01:04 +0200

    feat: add product routes

---
+++ server/routes/productRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createProduct,
+  getProduct,
+  getAllProducts,
+  updateProduct,
+  deleteProduct,
+} = require("../controller/productCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+
+router.post("/", authMiddleware, isAdmin, createProduct);
+router.get("/:id", getProduct);
+router.put("/:id", authMiddleware, isAdmin, updateProduct);
+router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
+router.get("/", getAllProducts);
+
+module.exports = router;
