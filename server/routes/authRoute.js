36cfcd7544dd144305a9c88e33c5629f28f2699c
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2023-04-20 16:21:10 +0200

    feat: add user wishlist & admin login routes

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -13,6 +13,8 @@ const {
   updatePassword,
   forgotPasswordToken,
   resetPassword,
+  loginAdmin,
+  getWishlist,
 } = require("../controller/userCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
@@ -23,11 +25,14 @@ router.post("/forgot-password-token", forgotPasswordToken);
 router.put("/reset-password/:token", resetPassword);
 
 router.post("/login", loginUserCtrl);
+router.post("/admin-login", loginAdmin);
+
 router.get("/all-users", getallUser);
 router.get("/refresh", handleRefreshToken);
 router.get("/logout", logout);
-
+router.get("/wishlist", authMiddleware, getWishlist);
 router.get("/:id", authMiddleware, isAdmin, getaUser);
+
 router.delete("/:id", deleteaUser);
 router.put("/edit-user", authMiddleware, updatedUser);
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
