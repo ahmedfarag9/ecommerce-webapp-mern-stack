309b0e4b89346cebb7da837ece819178d3cc869f
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2023-05-04 17:23:49 +0300

    feat: add user-Enquiry routes

---
+++ server/routes/enqRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createEnquiry,
+  updateEnquiry,
+  deleteEnquiry,
+  getEnquiry,
+  getAllEnquiry,
+} = require("../controller/enqCtrl");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
+
+router.post("/", createEnquiry);
+router.put("/:id", authMiddleware, isAdmin, updateEnquiry);
+router.delete("/:id", authMiddleware, isAdmin, deleteEnquiry);
+router.get("/:id", getEnquiry);
+router.get("/", getAllEnquiry);
+
+module.exports = router;
