49aa9e1cfef7e15c6b08bedf10ac93b6825cf42a
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2023-06-01 20:02:36 +0300

    feat: add auth pages

--- admin-dashboard/src/pages/Forgotpassword.js
+++ admin-dashboard/src/pages/Forgotpassword.js
@@ -10,7 +10,7 @@ const Forgotpassword = () => {
       <br />
       <br />
       <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
-        <h3 className="text-center">Forgot Password</h3>
+        <h3 className="text-center title">Forgot Password</h3>
         <p className="text-center">
           Please enter your email address and we will send you a link to reset
           your password.
