9b3caa259427122b397f47432808a667a4cd26cd
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2023-07-13 15:26:45 +0300

    feat: add redux to dashboard pages

--- admin-dashboard/src/pages/Brandlist.js
+++ admin-dashboard/src/pages/Brandlist.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Table } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { getBrands } from "../features/brand/brandSlice";
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const columns = [
   {
@@ -9,33 +14,46 @@ const columns = [
   {
     title: "Name",
     dataIndex: "name",
+    sorter: (a, b) => a.name.length - b.name.length,
   },
   {
-    title: "Product",
-    dataIndex: "product",
-  },
-  {
-    title: "Status",
-    dataIndex: "status",
+    title: "Action",
+    dataIndex: "action",
   },
 ];
 
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-  data1.push({
-    key: i,
-    name: `Edward King ${i}`,
-    product: 32,
-    status: `London, Park Lane no. ${i}`,
-  });
-}
-
 const Brandlist = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBrands());
+  }, [dispatch]);
+
+  const brandState = useSelector((state) => state.brand.brands.brands);
+  const data = [];
+  if (brandState !== undefined) {
+    for (let i = 0; i < brandState.length; i++) {
+      data.push({
+        key: i + 1,
+        name: brandState[i].title,
+        action: (
+          <>
+            <Link className="fs-3 text-danger" to="/">
+              <BiEdit />
+            </Link>
+            <Link className="ms-3 fs-3 text-danger" to="/">
+              <AiFillDelete />
+            </Link>
+          </>
+        ),
+      });
+    }
+  }
+
   return (
     <div>
       <h3 className="mb-4 title">Brands</h3>
       <div>
-        <Table columns={columns} dataSource={data1} />
+        <Table columns={columns} dataSource={data} />
       </div>
     </div>
   );
